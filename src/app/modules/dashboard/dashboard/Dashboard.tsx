994e1796286e1db3621e23d5ec24cb2becfac41e
Ziya <ziyakaragoz.business@outlook.com>
2024-02-16 15:05:14 +0300

    update

--- src/app/modules/dashboard/dashboard/Dashboard.tsx
+++ src/app/modules/dashboard/dashboard/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
   return (
     <>
       <div>
-        <h1>Dashboard</h1>
+        <h1 className="text-3xl underline text-red-500">Dashboard</h1>
       </div>
     </>
   );
