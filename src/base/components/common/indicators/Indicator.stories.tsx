a1197a0f852f256a39f12e97e2f162921f1b1960
Ziya <ziyakaragoz.business@outlook.com>
2024-04-13 19:08:47 +0300

    storybook integrated and indicator documented as initial

---
+++ src/base/components/common/indicators/Indicator.stories.tsx
@@ -0,0 +1,76 @@
+import Indicator from "./Indicator";
+
+export default {
+  component: Indicator,
+  title: 'Indicator',
+  tags: ['autodocs'],
+};
+
+export const Default = {
+    args: {
+        children: "İşlem yapılıyor..."
+    },
+  };
+
+export const Blue = {
+    args: {
+        children: "İşlem yapılıyor...",
+        color: "blue"
+    },
+  };
+
+export const Gray = {
+    args: {
+        children: "İşlem yapılıyor...",
+        color: "gray"
+    },
+  };
+
+export const Red = {
+    args: {
+        children: "İşlem yapılıyor...",
+        color: "red"
+    },
+  };
+
+export const Green = {
+    args: {
+        children: "İşlem yapılıyor...",
+        color: "green"
+    },
+  };
+
+export const Yellow = {
+    args: {
+        children: "İşlem yapılıyor...",
+        color: "yellow"
+    },
+  };
+
+export const Indigo = {
+    args: {
+        children: "İşlem yapılıyor...",
+        color: "indigo"
+    },
+  };
+
+export const Purple = {
+    args: {
+        children: "İşlem yapılıyor...",
+        color: "purple"
+    },
+  };
+
+export const Pink = {
+    args: {
+        children: "İşlem yapılıyor...",
+        color: "pink"
+    },
+  };
+
+export const Orange = {
+    args: {
+        children: "İşlem yapılıyor...",
+        color: "orange"
+    },
+  };
